e72b37c4c0c5ea50cdbaee011bb5af274eb57b35
Omar Mohamed Yousef <omar.m.a.yousef@gmail.com>
2024-07-25 15:06:09 +0300

    Time: 204 ms (51.6%), Space: 98.1 MB (46.08%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        map<int,int> orderm;
+        for(auto num : nums)
+        {
+            ++orderm[num];
+        }
+        int counter = 0;
+        for(auto num : orderm)
+        {   
+            for(int i = 0;i<num.second;++i)
+            {   
+                nums[counter]=num.first;
+                ++counter;
+
+            }
+            
+        }
+        
         return nums;
     }
 };
